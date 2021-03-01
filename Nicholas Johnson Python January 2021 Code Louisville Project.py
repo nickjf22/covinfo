7572fce2be7841d01fb9f129158ce991c6ce840e
Nicholas Johnson <nickjf22@gmail.com>
2021-03-01 17:06:19 -0500

    Add main project file

---
+++ Nicholas Johnson Python January 2021 Code Louisville Project.py
@@ -0,0 +1,7 @@
+first_name = input("What is your name?  ")
+language="Python"
+print("Hello,", first_name +"!")
+print(first_name, "is learning {}.".format(language))
+
+
+print("Some other languages that", first_name + " has learned include {} and {}.".format("C#", "HTML"))
